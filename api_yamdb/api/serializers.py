28e18b29b811e9c4fec0b5f12f503fc9f71707c5
honeyVoid <116582081+honeyVoid@users.noreply.github.com>
2023-05-22 13:10:09 +0300

    added UserVieSet, UserSErializer, UserUPdateSerializer, updated models.py

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from reviews.models import User
 
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 
-from rest_framework import status, serializers, viewsets, pagination, permissions
+from rest_framework import (status, serializers, viewsets,
+                            pagination, permissions)
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 
